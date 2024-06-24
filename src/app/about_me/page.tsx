8b8548455374f9483ead095f60b0726053c5426b
Yasuhiro1443 <for.develop.and.analytics.yishida@gmail.com>
2024-06-24 10:21:42 +0900

    Add and Fix code

---
+++ src/app/about_me/page.tsx
@@ -0,0 +1,5 @@
+export default function AboutMe() {
+  return (
+    <h1>This is About Me page!!</h1>
+  );
+}
---
+++ src/app/works/page.tsx
@@ -0,0 +1,6 @@
+export default function Works() {
+    return (
+      <h1>This is Works page!!</h1>
+    );
+  }
+  
