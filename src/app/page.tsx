402841f03f8451f8ba4d36ad7df089420abe5cbf
Yasuhiro1443 <for.develop.and.analytics.yishida@gmail.com>
2024-06-25 17:29:14 +0900

    Update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-import Image from "next/image";
-import localImage from "../../public/test.png";
+import { Profile } from '../components/Profile'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* ここはProfile的なcomponentsを作るほうが良さそう、about meでflex-colの変化させる感じ */}
-      <div className="flex flex-col justify-center items-center gap-4">
-        <Image
-          src={localImage}
-          width={500}
-          height={500}
-          alt="Picture of the author"
-        />
-        <h1>Hello World!!</h1>
-      </div>
+      <Profile />
     </main>
   );
 }
