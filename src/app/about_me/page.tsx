b8c428d04bf0b005d6ea2c0ddf03ae45178186ae
Yasuhiro1443 <for.develop.and.analytics.yishida@gmail.com>
2024-06-26 19:06:28 +0900

    Update code

--- src/app/about_me/page.tsx
+++ src/app/about_me/page.tsx
@@ -1,9 +1,75 @@
 import { Profile } from '../../components/Profile'
+// これはどこかでデータを定義しておいて、for分とかで読み込む方式がいい
+import { Experience } from '../../components/Experience'
 
 export default function AboutMe() {
+  // const experiences = {
+  //   carrer: [
+  //     {
+  //       year: "2023年11月 - 現在",
+  //       jobTitle: "Data Scientist / Data Aanlyst",
+  //       companyName: "RAKSUL Inc. Data Strategy",
+  //       description: `
+  //         東京にてデータサイエンティストとして、現在は事業会社の課題や目的に合わせて、施策の立案から効果検証にかけて一気通貫して業務を行っています。
+  //         また、過去の経験からプロトタイプによる業務改善も手掛けており、分析や可視化・モデリングだけでなく、運用のための設計と実装も行っていました。
+  //       `
+  //     }
+  //   ]
+  // };
+  // 共通で使うならば、jobTitleとcompanyNameは命名規則として正しくない
+  // Skillsはこことは別のcomponentsになりそう
+  const experiences = {
+    carrer: [
+      {
+        year: "2023年11月 - 現在",
+        title: "Data Scientist / Data Aanlyst",
+        affiliationName: "RAKSUL Inc. Data Strategy",
+        description: `
+          東京にてデータサイエンティストとして、現在は事業会社の課題や目的に合わせて、施策の立案から効果検証にかけて一気通貫して業務を行っています。
+          また、過去の経験からプロトタイプによる業務改善も手掛けており、分析や可視化・モデリングだけでなく、運用のための設計と実装も行っていました。
+        `
+      },
+      {
+        year: "2022年7月 - 2023年11月",
+        title: "Data Scientist / Analytics Consaltant",
+        affiliationName: "Deloitte",
+        description: `
+          働いていた
+        `
+      }
+    ],
+    education: [
+      {
+        year: "2023年11月 - 現在",
+        title: "Data Scientist / Data Aanlyst",
+        affiliationName: "TEST",
+        description: ""
+      },
+    ]
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* Profileに合わせて下のやつもサイズをあわせたい */}
       <Profile />
+      <div className="flex flex-col justify-center items-top gap-2 p-5 w-[70%]">
+        {Object.keys(experiences).map(key => {
+            {/* この書き方はリファクタリングする必要あるかも */}
+            return experiences[key].map((experience, index) => {
+              return (
+                <div>
+                  {index == 0 && <h1 className="title text-5xl font-bold p-2">{key.charAt(0).toUpperCase() + key.slice(1)}</h1>}
+                  <Experience
+                    year={experience.year}
+                    title={experience.title}
+                    affiliationName={experience.affiliationName}
+                    description={experience.description}
+                  />
+                </div>
+              )
+            })
+        })}
+      </div>
     </main>
   );
 }
--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,14 +1,20 @@
 // どこにデータかjsonを定義しておいて、それを読み込む形式にしたい
-export const Experience: React.FC = () => {
+type Props = {
+  year: string,
+  title: string,
+  affiliationName: string,
+  description: string
+}
+
+// :TODO JSX.Elintと呼ばれる形にしているので、どれに統一すべきか考えたい 
+export const Experience = ({year, title, affiliationName, description}: Props) => {
     return (
       <main>
-        {/* <h1 className="title text-5xl font-bold p-2">Carrer</h1> */}
-        <h2 className="year p-1 text-neutral-400">2023年11月 - 現在</h2>
-        <h2 className="job-title p-1 text-gray-900 font-bold">Data Scientist / Data Analyst</h2>
-        <h2 className="company_name p-1 text-neutral-400 italic">RAKSUL Inc. Data Strategy</h2>
+        <h2 className="year p-1 text-neutral-400">{year}</h2>
+        <h2 className="jobTitle p-1 text-gray-900 font-bold">{title}</h2>
+        <h2 className="companyName p-1 text-neutral-400 italic">{affiliationName}</h2>
         <h2 className="description p-1">
-          東京にてデータサイエンティストとして、現在は事業会社の課題や目的に合わせて、施策の立案から効果検証にかけて一気通貫して業務を行っています。
-          また、過去の経験からプロトタイプによる業務改善も手掛けており、分析や可視化・モデリングだけでなく、運用のための設計と実装も行っていました。
+          {description}
         </h2>
       </main>
     );
