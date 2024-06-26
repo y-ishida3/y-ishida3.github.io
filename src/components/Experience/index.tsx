d9435116d2c174438cc0a56d8589aeb8ee2a58cc
Yasuhiro1443 <for.develop.and.analytics.yishida@gmail.com>
2024-06-26 19:25:04 +0900

    Fix code

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -11,8 +11,8 @@ export const Experience = ({year, title, affiliationName, description}: Props) =
     return (
       <main>
         <h2 className="year p-1 text-neutral-400">{year}</h2>
-        <h2 className="jobTitle p-1 text-gray-900 font-bold">{title}</h2>
-        <h2 className="companyName p-1 text-neutral-400 italic">{affiliationName}</h2>
+        <h2 className="title p-1 text-gray-900 font-bold">{title}</h2>
+        <h2 className="affiliationName p-1 text-neutral-400 italic">{affiliationName}</h2>
         <h2 className="description p-1">
           {description}
         </h2>
