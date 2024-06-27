b2a968ba4f5d0ace75cb2892dc0a3bf5d63b46a3
Yasuhiro1443 <for.develop.and.analytics.yishida@gmail.com>
2024-06-27 11:27:50 +0900

    Add code

---
+++ src/components/Skill/index.tsx
@@ -0,0 +1,30 @@
+// これがどういう意味があまりわかっていない
+// "use client";
+
+type Props = {
+  startDate: string,
+  iconName: string,
+  description: string
+}
+
+export const Skill = ({startDate, iconName, description}: Props) => {
+  const currentDate = new Date();
+  const startDate_ = new Date(startDate);
+  const experienceYear = (
+    // ミリ秒単位なので、ミリ秒->秒->分->時間->日->年に変換
+    // 少数第一位まで出力するために100をかけて100で割っている
+    Math.round(((currentDate - startDate_) / 1000 / 60 / 60 / 24 / 365 * 10)) / 10
+  ).toFixed(1);
+
+  return (
+    <div className="flex flex-row gap-1">
+      <div className="flex flex-col items-center justify-center text-3xl">
+        <img width="96" height="96" src={`https://img.icons8.com/color/96/${iconName}.png`} alt={iconName}/>
+      </div>
+      <div className="flex flex-col justify-center images-center gap-1">
+        <div className="text-xl font-bold">{experienceYear}年</div>
+        <div className="text-neutral-400 text-xs">{description}</div>
+      </div>
+    </div>
+  );
+};
