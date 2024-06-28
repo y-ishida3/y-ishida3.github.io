256cf52c5bf5075c24662af97b16f840806d96cf
Yasuhiro1443 <for.develop.and.analytics.yishida@gmail.com>
2024-06-28 12:03:22 +0900

    Remove file

--- src/app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Contact() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col justify-center items-center gap-4">
-        <h1 className="title text-5xl font-bold p-2">Comming Soon</h1>
-      </div>
-    </main>
-  );
-}
