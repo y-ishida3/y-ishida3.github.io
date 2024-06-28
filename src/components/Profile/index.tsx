8fc34b97f5b9d6e4c9d4bb4996deaac4cab9dfd2
Yasuhiro1443 <for.develop.and.analytics.yishida@gmail.com>
2024-06-28 12:03:45 +0900

    Update code

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -28,9 +28,17 @@ export const Profile: React.FC = () => {
         <h1 className="name text-5xl font-bold p-2">Yasuhiro Ishida</h1>
         <h2 className="job-title p-1 text-neutral-400">Data Scientist / Data Analyst</h2>
         <h2 className="description p-1">
-          東京にてデータサイエンティストとして、現在は事業会社の課題や目的に合わせて、施策の立案から効果検証にかけて一気通貫して業務を行っています。
+          東京でデータサイエンティストとして働いており、現在は事業会社に所属し課題や目的に合わせて、施策の立案から効果検証にかけて一気通貫して業務を行っています。
           また、過去の経験からプロトタイプによる業務改善も手掛けており、分析や可視化・モデリングだけでなく、運用のための設計と実装も行っていました。
         </h2>
+        <div className="flex flex-row justify-center item-center gap-4 py-3">
+          <a href="https://www.linkedin.com/in/yasuhiro-ishida-50498b242">
+            <img width="100" height="100" src="https://img.icons8.com/fluency/100/linkedin.png" alt="linkedin"/>
+          </a>
+          <a href="https://github.com/y-ishida3">
+            <img width="100" height="100" src="https://img.icons8.com/fluency-systems-filled/100/github.png" alt="github"/>
+          </a>
+        </div>
       </div>
     </div>
   );
