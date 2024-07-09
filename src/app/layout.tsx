e2e4cd989605c8423e3dd58a423131b2e2947902
Yasuhiro1443 <for.develop.and.analytics.yishida@gmail.com>
2024-07-09 10:52:35 +0900

    Update code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,20 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
 import { AOSInit } from '../lib/aos';
 import { GoogleAnalytics } from '../lib/gatag';
-import { Header } from "../components/Header";
-import { Footer } from "../components/Footer";
+import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Home",
-  description: "y-ishida3 github pages",
+  title: 'Home',
+  description: 'y-ishida3 github pages',
+  verification: {
+    google: 'shKK7Veq-tVWZfS_UKqVPHDs80jYXMj_mKEa5OrAm1Y'
+  }
 };
 
 export default function RootLayout({
@@ -20,11 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   // :TODO 動的にdirectoryが変わったら定義できるようにしたい
-  const labels: Array<string> = ["Home", "About Me", "Works"];
-  const urls: Array<string> = ["/", "about_me", "works"];
+  const labels: Array<string> = ['Home', 'About Me', 'Works'];
+  const urls: Array<string> = ['/', 'about_me', 'works'];
 
   return (
-    <html lang="jp">
+    <html lang='jp'>
       <GoogleAnalytics />
       <AOSInit />
       <body className={inter.className}>
